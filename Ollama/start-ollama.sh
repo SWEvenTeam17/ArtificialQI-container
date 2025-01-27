1a80fd81b2cf1fdbc3e3e1015f780b0bd90b9e73
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2025-01-27 18:37:56 +0100

    Aggiunta installazione automatica llama3.2 3b

--- Ollama/start-ollama.sh
+++ Ollama/start-ollama.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 # start-ollama.sh
-
-# Start the Ollama service
 ollama serve &
-# Wait a bit to ensure the service starts
-sleep 10
-# Pull the model
-ollama pull moondream
-# Wait for the Ollama service to keep running
-wait
+sleep 2
+ollama pull llama3.2
